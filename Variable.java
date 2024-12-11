ac9a3ec01c741835a993f9919ab88adef4a36911
Harshil Dangar <145525815+Harshil-dangar-2208@users.noreply.github.com>
2024-12-11 16:38:08 +0530

    Create Variable.java
    using variable

---
+++ Variable.java
@@ -0,0 +1,23 @@
+public class Variable
+{
+	public static void main(String[] args) {
+    //Variable
+    String name = "Dangar";
+    int age = 19;
+    double salary = 80000;
+    
+    int a = 25;
+    int b = 10;
+    
+    
+    b = 20;
+    name = "Harshil";
+    
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(salary);
+
+	}
+}
+
+
